19af2c35f43bf6f527fe70bbd078d545306e4b3a
ColauttiLab <robert.colautti@queensu.ca>
2020-01-15 10:30:04 -0500

    Added plotting theme (theme_CLab.R) and link to blog on peer review

---
+++ theme_CLab.R
@@ -0,0 +1,16 @@
+# Clean theme for presentations & publications used in the Colautti Lab
+theme_CLab <- function (base_size = 12, base_family = "") {
+  theme_classic(base_size = base_size, base_family = base_family) %+replace% 
+    theme(
+      axis.text = element_text(colour = "black"),
+      axis.title.x = element_text(size=18),
+      axis.text.x = element_text(size=12),
+      axis.title.y = element_text(size=18,angle=90),
+      axis.text.y = element_text(size=12),
+      axis.ticks = element_blank(), 
+      panel.background = element_rect(fill="white"),
+      panel.border = element_blank(),
+      plot.title=element_text(face="bold", size=24),
+      legend.position="none"
+    ) 
+}
